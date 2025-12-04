ea70619421d20f4621b86103f2ee3010a0d278fe
Михайло Горбань <mikegorban@MacBook-Air-Mihajlo.local>
2025-12-04 18:58:25 +0200

    add tests.py

---
+++ tests/tests_app.py
@@ -0,0 +1,23 @@
+import pytest
+from app import add, is_even
+
+def test_add_simple():
+    assert add(2, 3) == 5
+    assert add(10, 5) == 15
+    assert add(-1, 1) == 0
+
+@pytest.mark.parametrize("value, expected", [
+    (2, True),
+    (3, False),
+    (10, True),
+])
+def test_is_even(value, expected):
+    assert is_even(value) == expected
+
+@pytest.fixture
+def sample_numbers():
+    return [2, 4, 6]
+
+def test_all_even(sample_numbers):
+    for n in sample_numbers:
+        assert is_even(n)
