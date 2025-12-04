634a626f8300c0f96aa7f35849145074e11a2810
Михайло Горбань <mikegorban@MacBook-Air-Mihajlo.local>
2025-12-04 19:08:58 +0200

    fix test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,18 @@
+import os
+import sys
 import pytest
+
+
+sys.path.insert(0, os.path.dirname(os.path.dirname(__file__)))
+
 from app import add, is_even
+
 
 def test_add_simple():
     assert add(2, 3) == 5
     assert add(10, 5) == 15
     assert add(-1, 1) == 0
+
 
 @pytest.mark.parametrize("value, expected", [
     (2, True),
@@ -14,9 +22,11 @@ def test_add_simple():
 def test_is_even(value, expected):
     assert is_even(value) == expected
 
+
 @pytest.fixture
 def sample_numbers():
     return [2, 4, 6]
+
 
 def test_all_even(sample_numbers):
     for n in sample_numbers:
